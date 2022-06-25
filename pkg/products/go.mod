23e970ed24ac154ca1c38d5de046f85bd10c9103
Salvatore Gonda <salvatore.gonda@gmail.com>
2022-06-25 17:03:05 +0200

    Add products package and templates

---
+++ pkg/products/go.mod
@@ -0,0 +1,10 @@
+module github.com/deemount/shobber/pkg/products
+
+go 1.18
+
+require (
+	github.com/deemount/shobber-tpl/pkg/models v0.0.0-20220625125635-0d543b4e018e
+	github.com/deemount/shobber-tpl/pkg/view v0.0.0-20220625125635-0d543b4e018e
+	github.com/deemount/shobber/pkg/meta v0.0.0-20220625130751-b3360efef575
+	github.com/deemount/shobber/pkg/navbar v0.0.0-20220625130751-b3360efef575
+)
---
+++ pkg/products/main.go
@@ -0,0 +1,38 @@
+package products
+
+import (
+	"net/http"
+
+	"github.com/deemount/shobber-tpl/pkg/models"
+	"github.com/deemount/shobber-tpl/pkg/view"
+	"github.com/deemount/shobber/pkg/meta"
+	"github.com/deemount/shobber/pkg/navbar"
+)
+
+var tplDir string = "../../lib/ui/html"
+var products *view.View
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	tmpls := []string{
+		tplDir + "/base.tmpl",
+		tplDir + "/pages/products/header.tmpl",
+		tplDir + "/pages/products/main.tmpl",
+		tplDir + "/pages/products/footer.tmpl",
+	}
+
+	data := models.Data{
+		PageTitle:       "Shobber:Products",
+		PageAuthor:      "Salvatore Gonda",
+		PageDescription: "A simple online shop platform",
+		OG:              meta.OpenGraph(),
+		NavBarLinks:     navbar.NavBar(),
+	}
+
+	products = view.NewView(tplDir, "base", tmpls)
+
+	err := products.RenderData(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
